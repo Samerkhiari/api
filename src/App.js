90c342ace3c898a15e9301350917d744c3e43bfa
samerkhiari <127328803+Samerkhiari@users.noreply.github.com>
2023-06-16 01:30:54 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Table from './Table'
+import UsersList from './UsersList';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Table/> */}
+    <UsersList/>
+    </div>
+  );
+}
+
+export default App;
